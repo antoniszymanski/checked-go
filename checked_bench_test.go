22affc75db69d5ec56a610d8522c9a3c53c9404c
Antoni Szymański <antoni.szymanski+github@proton.me>
2025-09-20 18:30:14 +0200

    test: add tests for Min and Max, reorganize benchmark and test code

--- checked_bench_test.go
+++ checked_bench_test.go
@@ -14,60 +14,54 @@ func init() {
 	os.Args = append(os.Args, "-test.benchtime=1x")
 }
 
-func BenchmarkAddBits(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(bits.Add64(a, b, 0))
-	}
+func Benchmark(b *testing.B) {
+	b.Run("Add", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(Add(a, b))
+		}
+	})
+	b.Run("bits.Add64", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(bits.Add64(a, b, 0))
+		}
+	})
+	b.Run("Sub", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(Sub(a, b))
+		}
+	})
+	b.Run("bits.Sub64", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(bits.Sub64(a, b, 0))
+		}
+	})
+	b.Run("Mul", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(Mul(a, b))
+		}
+	})
+	b.Run("mulUint64", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(mulUint64(a, b))
+		}
+	})
+	b.Run("bits.Mul64", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(bits.Mul64(a, b))
+		}
+	})
+	b.Run("Div", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(Div(a, b))
+		}
+	})
+	b.Run("bits.Div64", func(b *testing.B) {
+		for a, b := range cartesianIndices[uint64](1, 10_000) {
+			keepAlive(bits.Div64(0, a, b))
+		}
+	})
 }
 
-func BenchmarkAdd(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(Add(a, b))
-	}
-}
-
-func BenchmarkSubBits(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(bits.Sub64(a, b, 0))
-	}
-}
-
-func BenchmarkSub(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(Sub(a, b))
-	}
-}
-
-func BenchmarkMulBits(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(bits.Mul64(a, b))
-	}
-}
-
-func BenchmarkMul(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(Mul(a, b))
-	}
-}
-
-func BenchmarkMul64(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(mulUint64(a, b))
-	}
-}
-
-func BenchmarkDivBits(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(bits.Div64(0, a, b))
-	}
-}
-
-func BenchmarkDiv(b *testing.B) {
-	for a, b := range numbers[uint64](1, 10_000) {
-		use(Div(a, b))
-	}
-}
-
-func use(args ...any) {
+func keepAlive(args ...any) {
 	runtime.KeepAlive(args)
 }
--- checked_test.go
+++ checked_test.go
@@ -10,118 +10,153 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	var errors int
-	for a, b := range numbers(Min[int8](), Max[int8]()) {
-		if errors >= 10 {
-			break
-		}
-		actual, actualOk := Add(a, b)
-		expected := int64(a) + int64(b)
-		expectedOk := inRange(&expected)
-		if actualOk != expectedOk {
-			t.Errorf("%v + %v should equal (%v, %v), not (%v, %v)", a, b, expected, expectedOk, actual, actualOk)
-			errors++
-		}
-	}
+	test(t, Add, "+", func(a int64, b int64) int64 { return a + b })
 }
 
 func TestSub(t *testing.T) {
-	var errors int
-	for a, b := range numbers(Min[int8](), Max[int8]()) {
-		if errors >= 10 {
-			break
-		}
-		actual, actualOk := Sub(a, b)
-		expected := int64(a) - int64(b)
-		expectedOk := inRange(&expected)
-		if actualOk != expectedOk {
-			t.Errorf("%v - %v should equal (%v, %v), not (%v, %v)", a, b, expected, expectedOk, actual, actualOk)
-			errors++
-		}
-	}
+	test(t, Sub, "-", func(a int64, b int64) int64 { return a - b })
 }
 
 func TestMul(t *testing.T) {
-	var errors int
-	for a, b := range numbers(Min[int8](), Max[int8]()) {
-		if errors >= 10 {
-			break
-		}
-		actual, actualOk := Mul(a, b)
-		expected := int64(a) * int64(b)
-		expectedOk := inRange(&expected)
-		if actualOk != expectedOk {
-			t.Errorf("%v * %v should equal (%v, %v), not (%v, %v)", a, b, expected, expectedOk, actual, actualOk)
-			errors++
-		}
-	}
-
-	type TestCase struct {
-		A, B, C int64
-		OK      bool
-	}
-	for _, tc := range []TestCase{
-		{
-			A:  math.MinInt64 / 2,
-			B:  2,
-			C:  math.MinInt64,
-			OK: true,
+	testWithCases(t, Mul, Mul, "*", func(a int64, b int64) int64 { return a * b },
+		TestCase{
+			A:    math.MinInt64 / 2,
+			B:    2,
+			Want: T2(int64(math.MinInt64), true),
 		},
-		{
-			A:  math.MinInt64,
-			B:  1,
-			C:  math.MinInt64,
-			OK: true,
+		TestCase{
+			A:    math.MinInt64,
+			B:    1,
+			Want: T2(int64(math.MinInt64), true),
 		},
-		{
-			A:  math.MinInt64,
-			B:  -1,
-			OK: false,
-		},
-	} {
-		c, ok := Mul(tc.A, tc.B)
-		if ok != tc.OK {
-			t.Errorf("%v * %v should equal (%v, %v), not (%v, %v)", tc.A, tc.B, tc.C, tc.OK, c, ok)
-		}
-	}
+		TestCase{
+			A: math.MinInt64,
+			B: -1,
+		})
 }
 
 func TestDiv(t *testing.T) {
+	test(t, Div, "/", func(a int64, b int64) int64 { return a / b })
+}
+
+func TestMin(t *testing.T) {
+	assertEqual(t, "Min[uint8]()", Min[uint8](), 0)
+	assertEqual(t, "Min[uint16]()", Min[uint16](), 0)
+	assertEqual(t, "Min[uint32]()", Min[uint32](), 0)
+	assertEqual(t, "Min[uint64]()", Min[uint64](), 0)
+	assertEqual(t, "Min[int8]()", Min[int8](), math.MinInt8)
+	assertEqual(t, "Min[int16]()", Min[int16](), math.MinInt16)
+	assertEqual(t, "Min[int32]()", Min[int32](), math.MinInt32)
+	assertEqual(t, "Min[int64]()", Min[int64](), math.MinInt64)
+}
+
+func TestMax(t *testing.T) {
+	assertEqual(t, "Max[uint8]()", Max[uint8](), math.MaxUint8)
+	assertEqual(t, "Max[uint16]()", Max[uint16](), math.MaxUint16)
+	assertEqual(t, "Max[uint32]()", Max[uint32](), math.MaxUint32)
+	assertEqual(t, "Max[uint64]()", Max[uint64](), math.MaxUint64)
+	assertEqual(t, "Max[int8]()", Max[int8](), math.MaxInt8)
+	assertEqual(t, "Max[int16]()", Max[int16](), math.MaxInt16)
+	assertEqual(t, "Max[int32]()", Max[int32](), math.MaxInt32)
+	assertEqual(t, "Max[int64]()", Max[int64](), math.MaxInt64)
+}
+
+func test(
+	t *testing.T,
+	fn func(int8, int8) (int8, bool),
+	opName string, op func(a, b int64) int64,
+) {
 	var errors int
-	for a, b := range numbers(Min[int8](), Max[int8]()) {
+	for a, b := range int8s {
 		if errors >= 10 {
 			break
 		}
-		actual, actualOk := Div(a, b)
-		var expected int64
-		var expectedOk bool
-		if b != 0 {
-			expected = int64(a) / int64(b)
-			expectedOk = inRange(&expected)
-		}
-		if actualOk != expectedOk {
-			t.Errorf("%v / %v should equal (%v, %v), not (%v, %v)", a, b, expected, expectedOk, actual, actualOk)
+		got := T2(fn(a, b))
+		want := T2(expected(a, b, op))
+		if got != want {
+			t.Errorf("%v %s %v: got %v want %v", a, opName, b, got, want)
 			errors++
 		}
 	}
 }
 
-func inRange(x *int64) bool {
-	ok := math.MinInt8 <= *x && *x <= math.MaxInt8
-	if !ok {
-		*x = 0
+func testWithCases(
+	t *testing.T,
+	fn8 func(int8, int8) (int8, bool), fn64 func(int64, int64) (int64, bool),
+	opName string, op func(a, b int64) int64,
+	cases ...TestCase,
+) {
+	var errors int
+	for a, b := range int8s {
+		if errors >= 10 {
+			break
+		}
+		got := T2(fn8(a, b))
+		want := T2(expected(a, b, op))
+		if got != want {
+			t.Errorf("%v %s %v: got %v want %v", a, opName, b, got, want)
+			errors++
+		}
 	}
-	return ok
+	for _, tc := range cases {
+		if errors >= 10 {
+			break
+		}
+		got := T2(fn64(tc.A, tc.B))
+		if got != tc.Want {
+			t.Errorf("%v %s %v: got %v want %v", tc.A, opName, tc.B, got, tc.Want)
+			errors++
+		}
+	}
 }
 
-func numbers[T Integer](min, max T) iter.Seq2[T, T] {
+func assertEqual[T comparable](t *testing.T, op string, want, got T) {
+	if got != want {
+		t.Errorf("%s should equal (%v), not (%v)", op, got, want)
+	}
+}
+
+func int8s(yield func(int8, int8) bool) {
+	cartesianIndices(int8(math.MinInt8), math.MaxInt8)(yield)
+}
+
+func cartesianIndices[T Integer](min, max T) iter.Seq2[T, T] {
 	return func(yield func(T, T) bool) {
-		for a := min; a < max; a++ {
-			for b := min; b < max; b++ {
+		for a := min; ; a++ {
+			for b := min; ; b++ {
 				if !yield(a, b) {
 					return
 				}
+				if b >= max {
+					break
+				}
+			}
+			if a >= max {
+				break
 			}
 		}
 	}
+}
+
+func expected(a, b int8, op func(a, b int64) int64) (int8, bool) {
+	defer func() { _ = recover() }()
+	c := op(int64(a), int64(b))
+	if math.MinInt8 <= c && c <= math.MaxInt8 {
+		return int8(c), true
+	}
+	return 0, false
+}
+
+type TestCase struct {
+	A, B int64
+	Want Tuple2[int64, bool]
+}
+
+type Tuple2[A, B any] struct {
+	A A
+	B B
+}
+
+func T2[A, B any](a A, b B) Tuple2[A, B] {
+	return Tuple2[A, B]{A: a, B: b}
 }
